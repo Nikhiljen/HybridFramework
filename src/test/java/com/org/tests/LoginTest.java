88170b406e188137c4349ca8edb788005e5f7134
Nikhiljen <npjengte@gmail.com>
2025-09-18 15:55:00 +0530

    Auto commit on 2025-09-18 15:54:59

--- src/test/java/com/org/tests/LoginTest.java
+++ src/test/java/com/org/tests/LoginTest.java
@@ -18,7 +18,7 @@ public class LoginTest extends Base {
     LoginPage loginPage;
     private static final Logger logger = LoggerHelper.getLogger(LoginTest.class);
 
-    //Run this before every test case to intilise driver
+    //Run this before every test case to initialise driver
     @BeforeTest
     public void Setup(){
         System.setProperty("log4j.configurationFile", "src/main/resources/log4j2.xml");
@@ -55,7 +55,46 @@ public class LoginTest extends Base {
 
             //click on sign button and jump on home page
             HomePage homepage = loginPage.SignButton();
-            logger.info("User Jump to HomePage of aplication", selectRole);
+            logger.info("User Jump to HomePage of application", selectRole);
+
+        } catch (AssertionError ae) {
+            logger.error("TestCase_101 FAILED: Assertion failed", ae);
+            throw ae;  // rethrow to let TestNG handle failure
+        } catch (Exception e) {
+            logger.error("TestCase_101 FAILED: Unexpected exception occurred", e);
+            Assert.fail("TestCase_101 FAILED due to exception: " + e.getMessage());
+        }
+    }
+
+    public void TestCase_102(){
+        String userName = configReader.getProperty("username");
+        String passWord = configReader.getProperty("password");
+        String usertype = configReader.getProperty("user_type");
+        String userRole = configReader.getProperty("user_role");
+
+        try{
+            //Enter username and password
+            loginPage.inputUsername(userName);
+            loginPage.inputPassword(passWord);
+
+            //Enter role
+            loginPage.userType(usertype);
+
+            //Select User or admin role
+            loginPage.selectUserRole(userRole);
+            loginPage.AcceptModelIfPresent();
+
+            //from user select role which you want
+            String selectRole = loginPage.getuserRole();
+            Assert.assertEquals(selectRole, userRole,"Dropdown selection failed!");
+            logger.info("Dropdown selection verified: {}", selectRole);
+
+            //click on term and condition checkbox
+            loginPage.termAndConditonCheckBox(true);
+
+            //click on sign button and jump on home page
+            HomePage homepage = loginPage.SignButton();
+            logger.info("User Jump to HomePage of application", selectRole);
 
         } catch (AssertionError ae) {
             logger.error("TestCase_101 FAILED: Assertion failed", ae);
